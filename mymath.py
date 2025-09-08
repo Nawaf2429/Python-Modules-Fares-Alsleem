eb9a42726d1534aa0628f67aa80352812cd0dcd0
Farsxvt1 <alslymfars9@gmail.com>
2025-09-08 14:30:40 +0300

    Add files via upload

---
+++ mymath.py
@@ -0,0 +1,48 @@
+def add(x,y):
+    print( x+y)
+def sub(x,y):
+    print( x-y)
+def mul(x,y):
+    print(x * y)
+def div(x,y):
+  if y == 0:
+      print("Error: Division by zero is not allowed.") 
+  print(x / y)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# choose = input("Enter choice(1/2): ")
+# if choose == '1'.lower():
+#     add(x=float(input('enter your first number:')),y=float(input('enter your second number:')))
+#     print(add)
+# elif choose == '2'.lower():
+#     sub(x=float(input('enter your first number:')),y=float(input('enter your second number:')))
+#     print(sub)
+# else:
+#     print("Invalid input")    
